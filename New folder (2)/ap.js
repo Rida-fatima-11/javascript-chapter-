b31bb43bdd7696bea0afb4737418429f2315c74c
Rida-fatima-11 <168473185+Rida-fatima-11@users.noreply.github.com>
2024-06-05 20:15:07 +0500

    Add files via upload

---
+++ New folder (2)/ap.js
@@ -0,0 +1,23 @@
+var username ;
+var myname = "Rida Fatima";
+
+var massage = "HELLO WORD";
+alert(massage)
+
+alert("My Name Is Rida Fatima");
+alert("My Age Is 21 Year ");
+alert("Now I'm working on javascript");
+
+var usingname = alert("pizza/pizz/piz/pi/p");
+
+var email = "RidaFatima@gamil.com";
+alert("my email addres is " + email);
+
+var book = "A smarter way to learn JavaScript"
+alert("I am to learn from the book a smart way to learn javascript" + book);
+
+document.write("yah! i can write html content through javascript")
+
+var sign ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(sign);
+
